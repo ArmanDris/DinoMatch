7d49150c37e62da7fab451926d893d29c3bc135f
Arman D <adrismir@gmail.com>
2024-07-25 16:21:42 -0700

    Updated source code link

--- ui/src/lib/components/Header.svelte
+++ ui/src/lib/components/Header.svelte
@@ -7,6 +7,6 @@
     <a href="/"><h1 class="text-[24px] text-left leading-[2.5rem]"><h1>DINO MATCH</h1></a>
     <div class="text-[20px]">
         <a href="https://www.kaggle.com/datasets/ravirajsinh45/crop-and-weed-detection-data-with-bounding-boxes" class="px-2" target="_blank">DATASET<FontAwesomeIcon icon={faExternalLinkSquareAlt} class='ml-[6px] w-[1em] h-[1em] inline'/></a>
-        <a href="https://github.com/ArmanDris/DefaultDetector" class="px-2" target="_blank">SOURCE CODE<FontAwesomeIcon icon={faExternalLinkSquareAlt} class='ml-[6px] w-[1em] h-[1em] inline'/></a>
+        <a href="https://github.com/ArmanDris/DinoMatch" class="px-2" target="_blank">SOURCE CODE<FontAwesomeIcon icon={faExternalLinkSquareAlt} class='ml-[6px] w-[1em] h-[1em] inline'/></a>
     </div>
 </header>
